25dfc7161dc95ece602bb1c387ca84e5e2b1d329
Daniel GA <dgonzarb@gmail.com>
2023-01-30 12:42:07 +0100

    Problema 595

---
+++ 595.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <iomanip>
+#include <climits>
+#include <cmath>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+#include <algorithm>
+#pragma warning (disable : 4996)
+
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<bool> vb;
+/*-------------------------------------*/
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << n / 100 << '\n';
+    }
+    return 0;
+}
